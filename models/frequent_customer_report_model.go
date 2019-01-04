c4083dc181a27ef2432269c23f5d4a53cfb5bec7
nisiyu <nisiyu91@sina.com>
2019-01-04 19:11:54 +0800

    update

--- models/frequent_customer_report_model.go
+++ models/frequent_customer_report_model.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"reflect"
+	"time"
+)
 
 type FrequentCustomerReport struct {
 	BaseModel
@@ -14,3 +17,62 @@ type FrequentCustomerReport struct {
 }
 
 // 总人数，高频次数，低频次数，新客数，总到访间隔天数，总到访天数
+
+type FrequentCustomerHighTimeTable struct {
+	BaseModel
+	FrequentCustomerGroupID uint      `gorm:"index"`
+	Date                    time.Time `gorm:"type:date"`
+	PhaseOne                uint      `gorm:"type:integer"`
+	PhaseTwo                uint      `gorm:"type:integer"`
+	PhaseThree              uint      `gorm:"type:integer"`
+	PhaseFour               uint      `gorm:"type:integer"`
+	PhaseFive               uint      `gorm:"type:integer"`
+	PhaseSix                uint      `gorm:"type:integer"`
+	PhaseSeven              uint      `gorm:"type:integer"`
+	PhaseEight              uint      `gorm:"type:integer"`
+}
+
+var hourPhaseMap = map[int]string{
+	8:  "PhaseOne",
+	9:  "PhaseOne",
+	10: "PhaseTwo",
+	11: "PhaseTwo",
+	12: "PhaseThree",
+	13: "PhaseThree",
+	14: "PhaseFour",
+	15: "PhaseFour",
+	16: "PhaseFive",
+	17: "PhaseFive",
+	18: "PhaseSix",
+	19: "PhaseSix",
+	20: "PhaseSeven",
+	21: "PhaseSeven",
+	22: "PhaseEight",
+	23: "PhaseEight",
+}
+
+func TimeToPhase(captureAt time.Time) string {
+	return hourPhaseMap[captureAt.Hour()]
+}
+
+var phaseTitleMap = map[string]string{
+	"PhaseOne":   "8:00-10:00",
+	"PhaseTwo":   "10:00-12:00",
+	"PhaseThree": "12:00-14:00",
+	"PhaseFour":  "14:00-16:00",
+	"PhaseFive":  "16:00-18:00",
+	"PhaseSix":   "18:00-20:00",
+	"PhaseSeven": "20:00-22:00",
+	"PhaseEight": "22:00-24:00",
+}
+
+func (table *FrequentCustomerHighTimeTable) AddCount(captureAt time.Time) {
+	phase := TimeToPhase(captureAt)
+	if phase == "" {
+		return
+	}
+	nowCount := reflect.ValueOf(table).Elem().FieldByName(phase).Int()
+	nowCount++
+
+	reflect.ValueOf(table).Elem().FieldByName(phase).SetInt(nowCount)
+}
--- models/frequent_customer_rule_model.go
+++ models/frequent_customer_rule_model.go
@@ -1,10 +1,71 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm/dialects/postgres"
+)
+
 type FrequentCustomerRule struct {
 	BaseModel
-	CompanyID       uint   `gorm:"index"`
-	RuleType        string `gorm:"type:varchar(16);"`
-	From            int    `gorm:"type:integer" json:"from"`
-	To              int    `gorm:"type:integer" json:"to"`
-	isHighFrequency bool   `gorm:"type:boolean"`
+	CompanyID     uint           `gorm:"index"`
+	LowFrequency  postgres.Jsonb `gorm:"jsonb"`
+	HighFrequency postgres.Jsonb `gorm:"jsonb"`
+	readableRules ReadableFrequencyRule
+}
+type ReadableFrequencyRule struct {
+	LowFrequency  []rulePair
+	HighFrequency []rulePair
+	Limit         uint
+}
+
+type rulePair struct {
+	From uint   `json:"from"`
+	To   uint   `json:"to"`
+	Type string `json:"type"`
+}
+
+type FrequentCustomerRuleSerializer []rulePair
+
+func (rule *FrequentCustomerRule) ReadableRule() ReadableFrequencyRule {
+	if rule.readableRules.Limit != 0 {
+		return rule.readableRules
+	}
+
+	// if rule.ID == 0 {
+	// TODO: 从数据中解析出readableFrequencyRule的方法
+	lf := []rulePair{
+		rulePair{
+			From: 1,
+			To:   2,
+			Type: "low",
+		},
+	}
+
+	hf := []rulePair{
+		rulePair{
+			From: 3,
+			To:   3,
+			Type: "high",
+		},
+		rulePair{
+			From: 4,
+			To:   4,
+			Type: "high",
+		},
+		rulePair{
+			From: 5,
+			To:   5,
+			Type: "high",
+		},
+		rulePair{
+			From: 6,
+			To:   30,
+			Type: "high",
+		},
+	}
+	return ReadableFrequencyRule{
+		LowFrequency:  lf,
+		HighFrequency: hf,
+		Limit:         3,
+	}
+	// }
 }
