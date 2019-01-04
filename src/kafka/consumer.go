aab9e42f5301a6e6dfcc31500b06e82d179bc2b2
XieWei <wei.xie@readsense.cn>
2019-01-04 18:36:31 +0800

    feat: people save

--- src/kafka/consumer.go
+++ src/kafka/consumer.go
@@ -17,10 +17,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jinzhu/gorm"
-
-	"github.com/spf13/viper"
-
 	"github.com/bsm/sarama-cluster"
 )
 
@@ -34,19 +30,28 @@ var titanParams struct {
 	identificationURL string
 }
 
+var ruleNumber struct {
+	high int
+	low  int
+}
+
 func consumerInit() {
 	// todo: fix use config.fetchValue
-	host := viper.Get(configs.ENV + ".kafka.host")
-	port := viper.Get(configs.ENV + ".kafka.port")
-	groupName := viper.GetString(configs.ENV + ".kafka.group")
-	topic := viper.Get(configs.ENV + ".kafka.topic")
+	host := configs.FetchFieldValue("KAFKAHOST")
+	port := configs.FetchFieldValue("KAFKAPORT")
+	groupName := configs.FetchFieldValue("KAFKAGROUP")
+	topic := configs.FetchFieldValue("KAFKATOPIC")
 	log.Println(fmt.Sprintf("env: %s,host:%s, port: %s, groupID: %s, topic: %s", configs.ENV, host, port, groupName, topic))
 	CountFrequentConsumerParams.brokers = []string{fmt.Sprintf("%s:%s", host, port)}
 	CountFrequentConsumerParams.groupID = groupName
-	CountFrequentConsumerParams.topics = []string{topic.(string)}
+	CountFrequentConsumerParams.topics = []string{topic}
 
 	// todo: titan faces/identification
 	titanParams.identificationURL = fmt.Sprintf("http://" + configs.FetchFieldValue("TitanHOST") + "/faces/identification")
+
+	// todo: rule number
+	ruleNumber.high = 3
+	ruleNumber.low = 2
 }
 
 func CountFrequentConsumer() {
@@ -105,6 +110,7 @@ type Info struct {
 	ApiSecret string `json:"api_secret"`
 	FaceID    string `json:"face_id"`
 	GroupID   string `json:"group_id"`
+	PersonID  string `json:"person_id"`
 }
 
 func infoHandler(values []byte) {
@@ -166,73 +172,42 @@ func fetchDataByTitan(group *models.FrequentCustomerGroup, info Info) {
 	for _, value := range values.(titan.CandidateData).Candidates {
 		personIDs = append(personIDs, value.PersonID)
 	}
-	personIDHandler(group, personIDs)
+	personIDHandler(group, info, personIDs)
 
 }
 
 type result struct {
-	PersonID   string    `json:"person_id"`
-	CapturedAt time.Time `json:"captured_at"`
+	PersonID string    `json:"person_id"`
+	Day      time.Time `json:"day"`
 }
 
 type results []result
 
-func personIDHandler(group *models.FrequentCustomerGroup, personIDs []string) {
+func personIDHandler(group *models.FrequentCustomerGroup, info Info, personIDs []string) {
 	personIDString := strings.Join(personIDs, ",")
 	now := time.Now()
 	right := now.Format("2006-01-02 15:04:05")
 	left := now.AddDate(0, -1, 0).Format("2006-01-02 15:04:05")
+	sql := fmt.Sprintf(`SELECT person_id, date_trunc('day',max(capture_at)) as day FROM events WHERE person_id in (%s) AND capture_at BETWEEN %s AND %s ORDER BY capture_at desc`,
+		personIDString, left, right)
+
+	var resultsValues results
+
+	database.POSTGRES.Raw(sql).Scan(&resultsValues)
+
+	//personID
 	{
-		sql := fmt.Sprintf(`SELECT person_id, captured_at FROM events WHERE person_id in %s AND captured_at BETWEEN %s AND %s ORDER BY captured_at desc`,
-			personIDString, left, right)
-		var resultsValues results
-
-		database.POSTGRES.Raw(sql).Scan(&resultsValues)
-
-		var one models.FrequentCustomerReport
-		if dbError := database.POSTGRES.Where("frequent_customer_group_id = ?", group.ID).First(&one).Error; dbError != nil {
-			day, _ := time.Parse("2006-01-02 00:00:00", time.Now().Format("2006-01-02 00:00:00"))
-			one = models.FrequentCustomerReport{
+		var onePerson models.FrequentCustomerPeople
+		day, _ := time.Parse("2006-01-02 00:00:00", time.Now().Format("2006-01-02 00:00:00"))
+		if dbError := database.POSTGRES.Where("person_id = ? AND date = ?", info.PersonID, day).First(&onePerson).Error; dbError != nil {
+			onePerson = models.FrequentCustomerPeople{
+				PersonID:                info.PersonID,
 				FrequentCustomerGroupID: group.ID,
 				Date:                    day,
-				SumTimes:                uint(len(resultsValues)),
+				Interval:                uint(float64(time.Now().Sub(resultsValues[0].Day).Hours()/24) + 1),
+				Frequency:               uint(len(resultsValues)),
 			}
-			if len(resultsValues) > 3 {
-				one.HighFrequency = 1
-				one.SumInterval = uint(float64(time.Now().Sub(resultsValues[0].CapturedAt).Hours())/24 + 1)
-			} else if len(resultsValues) < 3 && len(resultsValues) != 0 {
-				one.LowFrequency = 1
-				one.SumInterval = uint(float64(time.Now().Sub(resultsValues[1].CapturedAt).Hours())/24 + 1)
-			} else {
-				one.NewComer = 1
-				one.SumInterval = 0
-			}
-
-			database.POSTGRES.Save(&one)
-		} else {
-			if one.ID != 0 {
-				database.POSTGRES.Model(&one).Updates()
-
-				if len(resultsValues) > 3 {
-					database.POSTGRES.Model(&one).Updates(map[string]interface{}{
-						"high_frequency": gorm.Expr("high_frequency + ?", 1),
-					})
-					one.SumInterval = uint(float64(time.Now().Sub(resultsValues[0].CapturedAt).Hours())/24 + 1)
-
-				} else if len(resultsValues) < 3 && len(resultsValues) != 0 {
-					database.POSTGRES.Model(&one).Updates(map[string]interface{}{
-						"low_frequency": gorm.Expr("low_frequency + ?", 1),
-					})
-					one.SumInterval = uint(float64(time.Now().Sub(resultsValues[1].CapturedAt).Hours())/24 + 1)
-
-				} else {
-					database.POSTGRES.Model(&one).Updates(map[string]interface{}{
-						"new_comer": gorm.Expr("new_comer + ?", 1),
-					})
-				}
-
-			}
+			database.POSTGRES.Save(&onePerson)
 		}
-
 	}
 }
