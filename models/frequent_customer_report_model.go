0f4bbdc94f3d0811824e6b97daa73feba57d0438
nisiyu <nisiyu91@sina.com>
2019-01-14 10:37:01 +0800

    fix high time table

--- models/frequent_customer_report_model.go
+++ models/frequent_customer_report_model.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"reflect"
+	"siren/pkg/database"
 	"siren/pkg/utils"
 	"time"
 )
@@ -73,54 +74,14 @@ func (table *FrequentCustomerHighTimeTable) AddCount(captureAt time.Time) {
 	if phase == "" {
 		return
 	}
-	nowCount := reflect.ValueOf(table).Elem().FieldByName(phase).Int()
+	nowCount := reflect.ValueOf(table).Elem().FieldByName(phase).Uint()
 	nowCount++
 
-	reflect.ValueOf(table).Elem().FieldByName(phase).SetInt(nowCount)
+	reflect.ValueOf(table).Elem().FieldByName(phase).SetUint(nowCount)
+
+	database.POSTGRES.Save(table)
 }
 
-// func InsertMissing(period string, fromTime time.Time, toTime time.Time, ) ([]ReportEventSerializer, int) {
-// 	duration := utils.GetDurationByPeriod(period)
-// 	var total int
-// 	if period == "month" {
-// 		total = (toTime.Year()-fromTime.Year())*12 + (int(toTime.Month()) - int(fromTime.Month())) + 1
-// 	} else {
-// 		total = GetSubByPeriod(period, fromTime, toTime, duration)
-// 	}
-// 	if total <= 0 {
-// 		return []ReportEventSerializer{}, 0
-// 	}
-// 	result := make([]ReportEventSerializer, total, total+1)
-
-// 	newTime := fromTime // init time
-// 	for i := range result {
-// 		result[i].Time = utils.CurrentTime(newTime, period)
-// 		for _, report := range reports {
-// 			left := report.Hour.UTC().Unix()
-// 			right := utils.CurrentTime(newTime, period).Unix()
-// 			if left == right {
-// 				result[i] = report.BaseSerialize()
-// 				break
-// 			}
-// 			if period == "year" {
-// 				result[i] = report.BaseSerialize()
-// 				break
-// 			}
-// 		}
-// 		if period == "month" {
-// 			month := int(newTime.Month())
-// 			day := newTime.Day()
-// 			nextYear := newTime.Year()
-// 			if month == 12 {
-// 				nextYear++
-// 			}
-// 			newTime = time.Date(nextYear, time.Month(month%12+1), day, 0, 0, 0, 0, time.Local)
-// 		} else {
-// 			newTime = newTime.Add(duration)
-// 		}
-// 	}
-// 	return result, len(result)
-// }
 type FrequentCustomerReports []FrequentCustomerReport
 
 func (reports FrequentCustomerReports) InsertMissing(period string, fromTime time.Time, toTime time.Time, sortBy string) ([]FrequentCustomerReport, error) {
--- src/workers/store_worker_test.go
+++ src/workers/store_worker_test.go
@@ -4,6 +4,7 @@ import (
 	"siren/configs"
 	"siren/initializers"
 	"siren/pkg/database"
+	"siren/pkg/utils"
 	"testing"
 	"time"
 )
@@ -14,5 +15,16 @@ func TestWorker(t *testing.T) {
 	database.DBinit()
 	// redis.Init()
 	defer database.POSTGRES.Close()
-	StoreFrequentCustomerHandler(241, 682, "2b68d6f2f6e684f19b0aaf1a9b61a883", time.Now().Unix())
+	StoreFrequentCustomerHandler(241, 682, "2b68d6f2f6e684f19b0aaf1a9b61a883", time.Now().Unix(), 12345)
+}
+
+func TestUpdateFrequentCustomerHighTimeTable(t *testing.T) {
+	configs.ENV = "dev"
+	initializers.ViperDefaultConfig()
+	database.DBinit()
+	// redis.Init()
+	defer database.POSTGRES.Close()
+	today := utils.CurrentDate(time.Now())
+	captureAt := time.Now().Unix()
+	updateFrequentCustomerHighTimeTable(2, today, captureAt)
 }
