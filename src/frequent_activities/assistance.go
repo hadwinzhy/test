265b25b38b455969193ba6d608e46843f532f8e1
XieWei <wei.xie@readsense.cn>
2019-01-16 17:11:54 +0800

    fix: bug

--- src/frequent_activities/assistance.go
+++ src/frequent_activities/assistance.go
@@ -14,8 +14,10 @@ func MakeResponse(context *gin.Context, code int, values interface{}) {
 func dateHandler(date string) (string, string) {
 	day := utils.TimestampToTime(date)
 	y, m, d := day.Date()
+	right := time.Date(y, m, d, 23, 59, 59, 59, time.Local)
 	left := time.Date(y, m, d, 0, 0, 0, 0, time.Local)
-	return left.Format("2006-01-02 15:04:05"), day.Format("2006-01-02 15:04:05")
+
+	return left.Format("2006-01-02 15:04:05"), right.Format("2006-01-02 15:04:05")
 }
 
 func monthHandler(date string) (string, string) {
